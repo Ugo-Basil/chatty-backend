de8231eee221e9f3fb5aa46e8ce59c5e391a09e0
Basil <ohuruogubasil@gmail.com>
2022-11-22 15:51:04 +0100

    added:user-cache-redis

--- src/features/auth/controllers/signUp.ts
+++ src/features/auth/controllers/signUp.ts
@@ -1,3 +1,5 @@
+import { UserCache } from './../../../shared/services/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
 import  HTTP_STATUS  from 'http-status-codes';
 import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
 import { ISignUpData } from './../interfaces/auth.interface';
@@ -10,6 +12,8 @@ import { signUpSchema } from '@auth/schemas/signUp';
 import { authService } from '@service/db/auth.service';
 import { Helpers } from '@global/helpers/helpers';
 import { uploads } from '@global/helpers/cloudinary-upload';
+
+const userCache: UserCache = new UserCache();
 
 export class SignUp {
   @joiValidation(signUpSchema)
@@ -39,6 +43,12 @@ export class SignUp {
       throw new BadRequestError('Error uploading image');
     }
 
+    //Add to redis
+    const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
+    userDataForCache.profilePicture = `https://res/cloudinary.com/duyvexdho/image/upload/v${result.version}/${userObjectId}`;
+
+    await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
+
     res.status(HTTP_STATUS.CREATED).json({
       status: 'success',
       message: 'User created successfully',
@@ -60,5 +70,42 @@ export class SignUp {
       createdAt: new Date(),
 
     } as IAuthDocument;
+  }
+  private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument{
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id: userObjectId,
+      authId: _id,
+      uId,
+      username: Helpers.firstletterToUpperCase(username),
+      email,
+      password,
+      avatarColor,
+      profilePicture: '',
+      blocked: [],
+      blockedBy: [],
+      work: '',
+      location: '',
+      school: '',
+      quote: '',
+      bgImageVersion: '',
+      bgImageId: '',
+      followersCount: 0,
+      followingCount: 0,
+      postsCount: 0,
+      notifications: {
+        messages: true,
+        reactions: true,
+        comments: true,
+        follows: true
+      },
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        youtube: ''
+      }
+
+    } as unknown as IUserDocument;
   }
 }
